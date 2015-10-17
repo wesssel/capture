0ded10381091817da283ced9260544b5c22991c2
wesselvanderpal@gmail.com <wesselvanderpal@gmail.com>
2015-10-17 12:36:28 +0200

    disable jquery error

--- capture.js
+++ capture.js
@@ -4,6 +4,7 @@ var system 	= require('system');
 var url 	= system.args[1];
 var size 	= system.args[2];
 
+/* screen width */
 if (size == 'phone') {
 	page.viewportSize = { width: 320, height: 568 };
 }
@@ -14,6 +15,18 @@ else{
 	page.viewportSize = { width: 1920, height: 1080 };
 }
 
+/* disable jQuery errors */
+page.onError = function(msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+    if (trace && trace.length) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+        });
+    }
+};
+
+/* clean file name */
 function slugify(txt){
     return txt
     .replace('http://', '')
@@ -25,6 +38,7 @@ function slugify(txt){
     .replace(/[^\w-]+/g,'');
 }
 
+/* image path */
 function imagePath(){
   if (!size) {
     size = 'desktop';
@@ -32,10 +46,12 @@ function imagePath(){
   return './images/'+slugify(url)+'_'+size+'.jpg';
 }
 
+/* capture page */
 function capture(){
 	return page.render(imagePath());	
 }
 
+/* open url and capture */
 page.open(url, function(status) {
   capture()
   console.log('Image can be found in: '+imagePath())
